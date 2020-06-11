875fa0ad7017fd1943dd64048a0da5c75334c2b9
Michael Ruocco <michael.ruocco@hotmail.com>
2020-06-11 02:06:55 +0100

    refactors to interface so private constructor is not required

--- domain/use-cases/src/main/java/uk/co/idv/context/usecases/identity/data/DurationCalculator.java
+++ domain/use-cases/src/main/java/uk/co/idv/context/usecases/identity/data/DurationCalculator.java
@@ -3,9 +3,9 @@ package uk.co.idv.context.usecases.identity.data;
 import java.time.Duration;
 import java.time.Instant;
 
-public class DurationCalculator {
+public interface DurationCalculator {
 
-    public static long millisBetween(final Instant start, final Instant end) {
+    static long millisBetween(final Instant start, final Instant end) {
         return Duration.between(start, end).toMillis();
     }
 
