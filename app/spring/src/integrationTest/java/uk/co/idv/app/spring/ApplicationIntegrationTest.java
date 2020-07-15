818c9c3c21c87f79d41b19422a7c7d1276185bdf
Michael Ruocco <michael.ruocco@hotmail.com>
2020-07-15 21:49:48 +0100

    adds assert to test

--- app/spring/src/integrationTest/java/uk/co/idv/app/spring/ApplicationIntegrationTest.java
+++ app/spring/src/integrationTest/java/uk/co/idv/app/spring/ApplicationIntegrationTest.java
@@ -3,13 +3,15 @@ package uk.co.idv.app.spring;
 import org.junit.jupiter.api.Test;
 import org.junitpioneer.jupiter.SetSystemProperty;
 
+import static org.assertj.core.api.Assertions.assertThatCode;
+
 public class ApplicationIntegrationTest {
 
     @Test
     @SetSystemProperty(key = "spring.profiles.active", value = "stubbed")
     @SetSystemProperty(key = "server.port", value = "0")
     void applicationShouldStartWithStubbedProfile() {
-        Application.main(new String[0]);
+        assertThatCode(() -> Application.main(new String[0])).doesNotThrowAnyException();
     }
 
 }
