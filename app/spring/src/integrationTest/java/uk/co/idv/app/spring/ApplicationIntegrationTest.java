7a12e2edbd5d53163eddacb60252f7783ea288a0
Michael Ruocco <michael.ruocco@hotmail.com>
2020-07-15 21:46:10 +0100

    makes stubbed profile test start on random port

--- app/spring/src/integrationTest/java/uk/co/idv/app/spring/ApplicationIntegrationTest.java
+++ app/spring/src/integrationTest/java/uk/co/idv/app/spring/ApplicationIntegrationTest.java
@@ -7,6 +7,7 @@ public class ApplicationIntegrationTest {
 
     @Test
     @SetSystemProperty(key = "spring.profiles.active", value = "stubbed")
+    @SetSystemProperty(key = "server.port", value = "0")
     void applicationShouldStartWithStubbedProfile() {
         Application.main(new String[0]);
     }
