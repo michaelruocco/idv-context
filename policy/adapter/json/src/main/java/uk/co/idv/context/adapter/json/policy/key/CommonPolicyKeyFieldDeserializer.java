c900f9c8e5a8e1e1feac0b9302dc4c05c9b8d68e
Michael Ruocco <michael.ruocco@hotmail.com>
2020-07-31 08:35:50 +0100

    converts to interface to remove need for private constructor

--- policy/adapter/json/src/main/java/uk/co/idv/context/adapter/json/policy/key/CommonPolicyKeyFieldDeserializer.java
+++ policy/adapter/json/src/main/java/uk/co/idv/context/adapter/json/policy/key/CommonPolicyKeyFieldDeserializer.java
@@ -7,25 +7,25 @@ import uk.co.mruoc.json.jackson.JsonNodeConverter;
 import java.util.Collection;
 import java.util.UUID;
 
-public class CommonPolicyKeyFieldDeserializer {
+public interface CommonPolicyKeyFieldDeserializer {
 
-    public static UUID extractId(JsonNode node) {
+    static UUID extractId(JsonNode node) {
         return UUID.fromString(node.get("id").asText());
     }
 
-    public static int extractPriority(JsonNode node) {
+    static int extractPriority(JsonNode node) {
         return node.get("priority").asInt();
     }
 
-    public static String extractChannelId(JsonNode node) {
+    static String extractChannelId(JsonNode node) {
         return node.get("channelId").asText();
     }
 
-    public static Collection<String> extractActivityNames(JsonNode node, JsonParser parser) {
+    static Collection<String> extractActivityNames(JsonNode node, JsonParser parser) {
         return JsonNodeConverter.toStringCollection(node.get("activityNames"), parser);
     }
 
-    public static Collection<String> extractAliasTypes(JsonNode node, JsonParser parser) {
+    static Collection<String> extractAliasTypes(JsonNode node, JsonParser parser) {
         return JsonNodeConverter.toStringCollection(node.get("aliasTypes"), parser);
     }
 
