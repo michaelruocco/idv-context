b879414d4b22a9786ede9d63bbc0291142e19837
Michael Ruocco <michael.ruocco@hotmail.com>
2020-08-28 08:29:07 +0100

    logs unused variable as temporary measure to resolve sonar issue in incomplete class

--- context/domain/use-cases/src/main/java/uk/co/idv/context/usecases/policy/ContextPolicyService.java
+++ context/domain/use-cases/src/main/java/uk/co/idv/context/usecases/policy/ContextPolicyService.java
@@ -1,11 +1,14 @@
 package uk.co.idv.context.usecases.policy;
 
+import lombok.extern.slf4j.Slf4j;
 import uk.co.idv.context.entities.context.create.CreateContextRequest;
 import uk.co.idv.context.entities.policy.ContextPolicy;
 
+@Slf4j
 public class ContextPolicyService {
 
     public ContextPolicy load(CreateContextRequest request) {
+        log.info("loading context with request {}", request);
         return new ContextPolicy();
     }
 
