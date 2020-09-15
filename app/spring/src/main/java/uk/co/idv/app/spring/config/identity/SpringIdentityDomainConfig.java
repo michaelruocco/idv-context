91272aded74fa08ce4ce4437884dd5c65e803aab
Michael Ruocco <michael.ruocco@hotmail.com>
2020-09-15 21:50:58 +0100

    attempts to fix better code hub warnings for duplicated code

--- app/spring/src/main/java/uk/co/idv/app/spring/config/identity/SpringIdentityDomainConfig.java
+++ app/spring/src/main/java/uk/co/idv/app/spring/config/identity/SpringIdentityDomainConfig.java
@@ -2,15 +2,12 @@ package uk.co.idv.app.spring.config.identity;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import uk.co.idv.identity.adapter.eligibility.external.ExternalFindIdentityStubConfig;
 import uk.co.idv.common.adapter.json.error.handler.ErrorHandler;
 import uk.co.idv.identity.config.IdentityConfig;
 import uk.co.idv.identity.config.repository.IdentityRepositoryConfig;
 import uk.co.idv.identity.usecases.eligibility.CreateEligibility;
 import uk.co.idv.identity.usecases.identity.IdentityService;
 
-import java.time.Duration;
-import java.util.concurrent.Executors;
 
 @Configuration
 public class SpringIdentityDomainConfig {
@@ -19,7 +16,6 @@ public class SpringIdentityDomainConfig {
     public IdentityConfig identityConfig(IdentityRepositoryConfig repositoryConfig) {
         return IdentityConfig.builder()
                 .repository(repositoryConfig.identityRepository())
-                .stubConfig(stubConfig())
                 .build();
     }
 
@@ -36,15 +32,6 @@ public class SpringIdentityDomainConfig {
     @Bean
     public ErrorHandler identityErrorHandler(IdentityConfig identityConfig) {
         return identityConfig.errorHandler();
-    }
-
-    private static ExternalFindIdentityStubConfig stubConfig() {
-        return ExternalFindIdentityStubConfig.builder()
-                .executor(Executors.newFixedThreadPool(2))
-                .timeout(Duration.ofMillis(250))
-                .phoneNumberDelay(Duration.ofMillis(400))
-                .emailAddressDelay(Duration.ofMillis(100))
-                .build();
     }
 
 }
