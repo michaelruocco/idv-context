c628cbf0a2c3cf93c159557ad5375e1d1d05916d
Michael Ruocco <michael.ruocco@hotmail.com>
2020-09-26 22:44:17 +0100

    refactors tests to removed duplication by making test parametized

--- context/adapter/sim-swap/stub/src/test/java/uk/co/idv/context/adapter/context/method/otp/delivery/phone/simswap/StubSimSwapResultFactoryTest.java
+++ context/adapter/sim-swap/stub/src/test/java/uk/co/idv/context/adapter/context/method/otp/delivery/phone/simswap/StubSimSwapResultFactoryTest.java
@@ -1,6 +1,8 @@
 package uk.co.idv.context.adapter.context.method.otp.delivery.phone.simswap;
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import uk.co.idv.context.entities.policy.method.otp.delivery.phone.OtpPhoneNumber;
 import uk.co.idv.context.entities.policy.method.otp.delivery.phone.simswap.SimSwapConfig;
 
@@ -8,6 +10,7 @@ import java.time.Clock;
 import java.time.Duration;
 import java.time.Instant;
 import java.time.ZoneId;
+import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.BDDMockito.given;
@@ -24,71 +27,39 @@ class StubSimSwapResultFactoryTest {
             .clock(clock)
             .build();
 
-    @Test
-    void shouldReturnFailureResultIfNumberEndsWith9() {
-        OtpPhoneNumber number = givenNumberWithLastDigit(9);
+    @ParameterizedTest
+    @MethodSource("provideLastDigitsAndExpectedStatusAndLastSwapped")
+    void shouldReturnSimSwapResultForLastDigitOfNumber(int lastDigit, String status, Instant lastSwapped) {
+        OtpPhoneNumber number = givenNumberWithLastDigit(lastDigit);
 
         SimSwapResult result = factory.build(number, config);
 
         assertThat(result)
-                .hasFieldOrPropertyWithValue("status", "failure")
+                .hasFieldOrPropertyWithValue("status", status)
                 .hasFieldOrPropertyWithValue("config", config)
-                .hasFieldOrPropertyWithValue("lastSwapped", null);
-    }
-
-    @Test
-    void shouldReturnUnknownResultIfNumberEndsWith8() {
-        OtpPhoneNumber number = givenNumberWithLastDigit(8);
-
-        SimSwapResult result = factory.build(number, config);
-
-        assertThat(result)
-                .hasFieldOrPropertyWithValue("status", "unknown")
-                .hasFieldOrPropertyWithValue("config", config)
-                .hasFieldOrPropertyWithValue("lastSwapped", null);
-    }
-
-    @Test
-    void shouldReturnUnknownResultIfNumberEndsWith7() {
-        OtpPhoneNumber number = givenNumberWithLastDigit(7);
-
-        SimSwapResult result = factory.build(number, config);
-
-        assertThat(result)
-                .hasFieldOrPropertyWithValue("status", "timeout")
-                .hasFieldOrPropertyWithValue("config", config)
-                .hasFieldOrPropertyWithValue("lastSwapped", null);
-    }
-
-    @Test
-    void shouldReturnSuccessResultWithLastSwapped5DaysAgoIfNumberEndsWith6() {
-        OtpPhoneNumber number = givenNumberWithLastDigit(6);
-
-        SimSwapResult result = factory.build(number, config);
-
-        Instant expectedLastSwapped = NOW.minus(Duration.ofDays(5));
-        assertThat(result)
-                .hasFieldOrPropertyWithValue("status", "success")
-                .hasFieldOrPropertyWithValue("config", config)
-                .hasFieldOrPropertyWithValue("lastSwapped", expectedLastSwapped);
-    }
-
-    @Test
-    void shouldReturnSuccessResultIfNumberEndsWith5() {
-        OtpPhoneNumber number = givenNumberWithLastDigit(5);
-
-        SimSwapResult result = factory.build(number, config);
-
-        assertThat(result)
-                .hasFieldOrPropertyWithValue("status", "success")
-                .hasFieldOrPropertyWithValue("config", config)
-                .hasFieldOrPropertyWithValue("lastSwapped", null);
+                .hasFieldOrPropertyWithValue("lastSwapped", lastSwapped);
     }
 
     private OtpPhoneNumber givenNumberWithLastDigit(int lastDigit) {
         OtpPhoneNumber number = mock(OtpPhoneNumber.class);
         given(number.getLastDigit()).willReturn(lastDigit);
         return number;
+    }
+
+    private static Stream<Arguments> provideLastDigitsAndExpectedStatusAndLastSwapped() {
+        final String success = "success";
+        return Stream.of(
+                Arguments.of(9, "failure", null),
+                Arguments.of(8, "unknown", null),
+                Arguments.of(7, "timeout", null),
+                Arguments.of(6, success, NOW.minus(Duration.ofDays(5))),
+                Arguments.of(5, success, null),
+                Arguments.of(4, success, null),
+                Arguments.of(3, success, null),
+                Arguments.of(2, success, null),
+                Arguments.of(1, success, null),
+                Arguments.of(0, success, null)
+        );
     }
 
 }
