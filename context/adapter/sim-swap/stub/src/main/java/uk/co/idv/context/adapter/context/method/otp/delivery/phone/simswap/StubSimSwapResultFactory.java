c7f324bfce1c8e68db0e9f7827ef1e396bd977d7
Michael Ruocco <michael.ruocco@hotmail.com>
2020-09-29 08:13:24 +0100

    refactors to shorten implementation

--- context/adapter/sim-swap/stub/src/main/java/uk/co/idv/context/adapter/context/method/otp/delivery/phone/simswap/StubSimSwapResultFactory.java
+++ context/adapter/sim-swap/stub/src/main/java/uk/co/idv/context/adapter/context/method/otp/delivery/phone/simswap/StubSimSwapResultFactory.java
@@ -2,59 +2,53 @@ package uk.co.idv.context.adapter.context.method.otp.delivery.phone.simswap;
 
 import lombok.Builder;
 import lombok.extern.slf4j.Slf4j;
-import uk.co.idv.context.adapter.context.method.otp.delivery.phone.simswap.SimSwapResult.SimSwapResultBuilder;
 import uk.co.idv.context.entities.policy.method.otp.delivery.phone.OtpPhoneNumber;
 import uk.co.idv.context.entities.policy.method.otp.delivery.phone.simswap.SimSwapConfig;
 
 import java.time.Clock;
 import java.time.Duration;
 import java.time.Instant;
+import java.util.Map;
 
 @Builder
 @Slf4j
 public class StubSimSwapResultFactory {
 
+    private static final Map<Integer, String> STATUSES = buildStatuses();
+
     private final Clock clock;
 
     public SimSwapResult build(OtpPhoneNumber number, SimSwapConfig config) {
         log.debug("building sim swap result for {}", number);
-        SimSwapResultBuilder builder = SimSwapResult.builder().config(config);
-        switch (number.getLastDigit()) {
-            case 9:
-                return toFailure(builder);
-            case 8:
-                return toUnknown(builder);
-            case 7:
-                return toTimeout(builder);
-            case 6:
-                return toSuccessWithLastSwapped(builder);
-            default:
-                return toSuccess(builder);
+        int lastDigit = number.getLastDigit();
+        return SimSwapResult.builder()
+                .config(config)
+                .status(toStatus(lastDigit))
+                .lastSwapped(toLastSwapped(lastDigit))
+                .build();
+    }
+
+    private String toStatus(int lastDigit) {
+        return STATUSES.getOrDefault(lastDigit, "success");
+    }
+
+    private Instant toLastSwapped(int lastDigit) {
+        if (lastDigit == 6) {
+            return calculateLastSwapped();
         }
-    }
-
-    private SimSwapResult toFailure(SimSwapResultBuilder builder) {
-        return builder.status("failure").build();
-    }
-
-    private SimSwapResult toUnknown(SimSwapResultBuilder builder) {
-        return builder.status("unknown").build();
-    }
-
-    private SimSwapResult toTimeout(SimSwapResultBuilder builder) {
-        return builder.status("timeout").build();
-    }
-
-    private SimSwapResult toSuccessWithLastSwapped(SimSwapResultBuilder builder) {
-        return toSuccess(builder.lastSwapped(calculateLastSwapped()));
+        return null;
     }
 
     private Instant calculateLastSwapped() {
         return clock.instant().minus(Duration.ofDays(5));
     }
 
-    private SimSwapResult toSuccess(SimSwapResultBuilder builder) {
-        return builder.status("success").build();
+    private static Map<Integer, String> buildStatuses() {
+        return Map.of(
+                9, "failure",
+                8, "unknown",
+                7, "timeout"
+        );
     }
 
 }
