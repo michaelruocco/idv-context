d4693c874e3d409940ef6af4f4b790ff1f69a48a
Michael Ruocco <michael.ruocco@hotmail.com>
2020-09-30 23:07:08 +0100

    adds tests for find delivery method method on context

--- app/manual/src/integrationTest/java/uk/co/idv/app/manual/context/otp/DeliveryMethodEligibleAndComplete.java
+++ app/manual/src/integrationTest/java/uk/co/idv/app/manual/context/otp/DeliveryMethodEligibleAndComplete.java
@@ -2,14 +2,12 @@ package uk.co.idv.app.manual.context.otp;
 
 import lombok.Builder;
 import lombok.Getter;
-import uk.co.idv.context.entities.context.method.otp.Otp;
 import uk.co.idv.context.entities.context.method.otp.delivery.DeliveryMethod;
 import uk.co.idv.context.usecases.context.ContextFacade;
 
 import java.util.UUID;
 import java.util.concurrent.Callable;
 
-import static uk.co.idv.context.entities.context.method.query.MethodQueryFactory.methodOfType;
 
 @Builder
 public class DeliveryMethodEligibleAndComplete implements Callable<Boolean> {
@@ -24,8 +22,7 @@ public class DeliveryMethodEligibleAndComplete implements Callable<Boolean> {
     @Override
     public Boolean call() {
         successful = contextFacade.find(contextId)
-                .find(methodOfType(Otp.class))
-                .flatMap(otp -> otp.findDeliveryMethod(deliveryMethodId))
+                .findDeliveryMethod(deliveryMethodId)
                 .filter(DeliveryMethod::isEligibilityComplete)
                 .map(DeliveryMethod::isEligible)
                 .orElse(false);
