57d37863f13a83c6869a14b714e8ce9e7d961bc1
Michael Ruocco <michael.ruocco@hotmail.com>
2020-10-10 16:12:15 +0100

    removes unused variable

--- context/adapter/json/src/main/java/uk/co/idv/context/adapter/json/context/sequence/SequenceDeserializer.java
+++ context/adapter/json/src/main/java/uk/co/idv/context/adapter/json/context/sequence/SequenceDeserializer.java
@@ -1,23 +1,16 @@
 package uk.co.idv.context.adapter.json.context.sequence;
 
 import com.fasterxml.jackson.core.JsonParser;
-import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.DeserializationContext;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.deser.std.StdDeserializer;
 import uk.co.idv.context.entities.context.method.Methods;
 import uk.co.idv.context.entities.context.sequence.Sequence;
-import uk.co.idv.method.entities.method.Method;
 import uk.co.mruoc.json.jackson.JsonNodeConverter;
 import uk.co.mruoc.json.jackson.JsonParserConverter;
 
-import java.util.Collection;
 
 class SequenceDeserializer extends StdDeserializer<Sequence> {
-
-    private static final TypeReference<Collection<Method>> METHOD_COLLECTION = new TypeReference<>() {
-        // intentionally blank
-    };
 
     protected SequenceDeserializer() {
         super(Sequence.class);
