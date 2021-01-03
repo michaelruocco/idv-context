921e7a160f831197f4588d14c29140a6aa56810a
Michael Ruocco <michael.ruocco@hotmail.com>
2021-01-03 08:58:16 +0000

    removes todo after adding karate test for get endpoint

--- app/manual/src/main/java/uk/co/idv/app/manual/Application.java
+++ app/manual/src/main/java/uk/co/idv/app/manual/Application.java
@@ -165,7 +165,6 @@ public class Application {
         return contextFacade.create(request);
     }
 
-    //TODO test
     public Verification get(GetVerificationRequest request) {
         return contextFacade.get(request);
     }
