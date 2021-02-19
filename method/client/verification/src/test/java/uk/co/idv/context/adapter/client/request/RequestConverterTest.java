cb599d7c73b0984189dd56b118fcb46eae2f20cb
Michael Ruocco <michael.ruocco@hotmail.com>
2021-02-19 18:35:58 +0000

    adds missing test coverage

--- method/client/verification/src/test/java/uk/co/idv/context/adapter/client/request/RequestConverterTest.java
+++ method/client/verification/src/test/java/uk/co/idv/context/adapter/client/request/RequestConverterTest.java
@@ -1,11 +1,14 @@
 package uk.co.idv.context.adapter.client.request;
 
 import org.junit.jupiter.api.Test;
+import uk.co.idv.context.adapter.client.exception.ClientException;
 import uk.co.mruoc.json.JsonConverter;
 
+import java.net.URISyntaxException;
 import java.net.http.HttpRequest;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.catchThrowable;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.mock;
 import static uk.co.idv.context.adapter.client.request.BodyExtractor.extractBody;
@@ -21,6 +24,23 @@ class RequestConverterTest {
             .baseUri(BASE_URI)
             .jsonConverter(jsonConverter)
             .build();
+
+    @Test
+    void shouldThrowExceptionIfBaseUrlIsInvalid() {
+        RequestConverter invalidConverter = RequestConverter.builder()
+                .baseUri(BASE_URI + " ")
+                .jsonConverter(jsonConverter)
+                .build();
+        ClientCreateVerificationRequest request = ClientCreateVerificationRequestMother.build();
+
+        Throwable error = catchThrowable(() -> invalidConverter.toPostVerificationHttpRequest(request));
+
+        assertThat(error)
+                .isInstanceOf(ClientException.class)
+                .hasCauseInstanceOf(URISyntaxException.class)
+                .hasMessage("java.net.URISyntaxException: Illegal character in authority at index 7: " +
+                        "http://localhost:8081 /v1/contexts/verifications");
+    }
 
     @Test
     void shouldConvertCreateVerificationRequestToHttpPostVerificationRequest() {
