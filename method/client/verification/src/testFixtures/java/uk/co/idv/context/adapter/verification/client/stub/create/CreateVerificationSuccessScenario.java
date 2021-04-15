1db41ab2ead8e64964c68f07128dc7f736a51e80
Michael Ruocco <michael.ruocco@hotmail.com>
2021-04-15 08:04:08 +0100

    refactors create verification success stub scenario to make it more configurable

--- method/client/verification/src/testFixtures/java/uk/co/idv/context/adapter/verification/client/stub/create/CreateVerificationSuccessScenario.java
+++ method/client/verification/src/testFixtures/java/uk/co/idv/context/adapter/verification/client/stub/create/CreateVerificationSuccessScenario.java
@@ -1,15 +1,11 @@
 package uk.co.idv.context.adapter.verification.client.stub.create;
 
 import lombok.Builder;
-import uk.co.idv.activity.entities.Activity;
 import uk.co.idv.context.adapter.verification.client.request.ClientCreateVerificationRequest;
-import uk.co.idv.method.entities.method.Methods;
 import uk.co.idv.method.entities.verification.Verification;
-import uk.co.idv.method.entities.verification.VerificationMother;
 
-import java.time.Clock;
 import java.time.Duration;
-import java.time.Instant;
+import java.util.function.Function;
 
 import static org.awaitility.Awaitility.await;
 
@@ -18,9 +14,7 @@ public class CreateVerificationSuccessScenario implements CreateVerificationScen
 
     @Builder.Default
     private final Duration delay = Duration.ZERO;
-    private final Clock clock;
-    private final Methods methods;
-    private final Activity activity;
+    private final Function<ClientCreateVerificationRequest, Verification> verificationFactory;
 
     @Override
     public boolean shouldExecute(ClientCreateVerificationRequest request) {
@@ -30,14 +24,7 @@ public class CreateVerificationSuccessScenario implements CreateVerificationScen
     @Override
     public Verification apply(ClientCreateVerificationRequest request) {
         await().pollDelay(delay).until(() -> true);
-        Instant created = clock.instant();
-        return VerificationMother.builder()
-                .contextId(request.getContextId())
-                .created(created)
-                .expiry(created.plus(methods.getLongestDuration()))
-                .activity(activity)
-                .methods(methods)
-                .build();
+        return verificationFactory.apply(request);
     }
 
 }
